aa365b884193ab68d10040d627e724113361deba
okas <lauri.saar@eesti.ee>
2017-01-15 17:13:31 +0200

    Working code: - flow rate calculation uses constant 15. Reason is that sensor is configured to impulse on both state change edges. - is flow rate sensor senses only one raising or falling edge then constant must be 7.5!
    NB! Currently, full setup (inc. valve) calculates error about 1-1,1%. Or is it sensor itself, dunno.
    You can reproduce it when commanding to pump 1000ml of water; it will pump 1011ml instead.
    100ml will pump 101ml.

---
+++ dev/valve.py
@@ -0,0 +1,11 @@
+from gpiozero import OutputDevice
+#~ from common import common_logger as log
+
+class Valve(OutputDevice):
+    def on(self):
+        super().on()
+        #~ log("  Valve is on.")
+        
+    def off(self):
+        super().off()
+        #~ log("  Valve is off.")
--- dev/water_flow_meter.py
+++ dev/water_flow_meter.py
@@ -1,95 +1,154 @@
-from RPi import GPIO
-from time import sleep, time
+from sys import stdin, stdout
+from time import sleep
+#~ from select import select
+from timeit import default_timer as timer
 from signal import pause
-from threading import Event
-from gpiozero import Button
+from threading import Event, Thread
+from gpiozero import OutputDevice, DigitalInputDevice
 from pumping import Pump
+from valve import Valve
+import faulthandler
+
+faulthandler.enable()
 
 
-pulse_count = 0
-stop_evt = Event()
-button = None
-pump = None
-t1 = t2 = None
+class DTO:
+    def __init__(self, **kwargs): self.__dict__ = kwargs
 
 
-def hall_pulse(channel):
-    global pulse_count, t2
-    pulse_count += 1
-    t2 = time()
-    work_time = t2 - t1
-    print("current speed: %.3f pl/sec; pulses: %d"\
-        % (pulse_count/work_time, pulse_count), flush=True, end='\r')
+class Devices():
+    def __init__(self, dto, hall_pulse_cb, args):
+        self.d = dto
+        self.vcc = OutputDevice(args.vcc_pin)
+        self.sensor = DigitalInputDevice(args.flow_pin)
+        self.sensor.when_activated = hall_pulse_cb
+        self.valve = Valve(args.valve_pin)
+        self.pump = Pump(args.pump_pin)
+
+    def __enter__(self): return self
+
+    def __exit__(self, t, v, tb):
+        self.sensor.when_activated = None
+        self.vcc.close()
+        self.sensor.close()
+        self.valve.close()
+        self.pump.close()
 
 
-def button_pressed(btn):
-    if pump.is_active: stop_evt.set()
-    else: stop_evt.clear()
-
-
-def setup_gpio(flow_pin, button_pin, pump_pin):
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(flow_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(flow_pin, GPIO.RISING, callback=hall_pulse, bouncetime=50)
-    global button, pump
-    button = Button(button_pin)
-    button.when_pressed = button_pressed
-    pump = Pump(pump_pin)
-
-
-def handle_result():
-    ml = float(input("Enter milliliters: "))
-    if not ml > 0:
-        print("Gave '%s' as millilitres value, cannot calculate with that!" % ml)
+def handle_result(dto):
+    try:
+        a_ml = float(input("Enter milliliters: "))
+        if not a_ml > 0: raise ValueError
+    except:
+        print("Gave '%s' as millilitres value, cannot calculate with that!" % a_ml)
         return
-    work_time = t2 - t1
-    ml_per_pl = ml / pulse_count
-    ml_in_sec = ml / work_time
-    l_in_min = (ml / 1000) / (work_time / 60)
-    pl_in_sec = pulse_count / work_time
-    pl_in_min = pulse_count / (work_time / 60)
-    print("===========================================================\nCalculations:")
-    print("  given values:")
-    print("    pulses: %18d" % pulse_count)
-    print("    reading time (sec): %6.3f" % work_time)
-    print("    millilitres: %13.3f" % ml)
+    time_elaps, pl_in_sec, lt_in_min, ml_sec_rate, total_vol_ml = calc_stat(dto)
+    pl_in_min = pl_in_sec * 60
+    ml_in_pl = total_vol_ml / dto.pulse_count
+    pl_in_lt = dto.pulse_count * 1000 / total_vol_ml
+    a_ml_in_pl = a_ml / dto.pulse_count
+    a_ml_in_sec = a_ml / time_elaps
+    a_lt_in_min = (a_ml / 1000) / (time_elaps / 60)
+    a_pl_in_lt = dto.pulse_count * 1000 / a_ml
+    print("===========================================================")
+    print("Calculations:")
+    print("  common values:")
+    print("    pulses: %19d" % dto.pulse_count)
+    print("    reading time (sec): %11.3f" % time_elaps)
+    print("    pulses per sec: %15.3f" % pl_in_sec)
+    print("    pulses per minute: %12.3f" % pl_in_min)
     print("-----------------------------------------------------------")
-    print("      waterflow: %.3f ml/p = %.3f ml / %d pulses" % (ml_per_pl, ml, pulse_count))
-    print("      ml per sec: %16.3f" % ml_in_sec)
-    print("      pl per sec: %16.3f" % pl_in_sec)
-    print("      litres per minute: %9.3f" % l_in_min)
-    print("      pulses per minute: %9.3f" % pl_in_min)
+    print("     actual millilitre-based stats:")
+    print("       total millilitres: %9.3f" % a_ml)
+    print("       ml per sec: %16.3f" % a_ml_in_sec)
+    print("       ml per pl: %17.3f" % a_ml_in_pl)
+    print("       litres per minute: %9.3f" % a_lt_in_min)
+    print("       pulses per litre: %10.3f" % a_pl_in_lt)
+    print("-----------------------------------------------------------")
+    print("     on-the-fly flow rate stats:")
+    print("       total millilitres: %9.3f" % total_vol_ml)
+    print("       ml per sec: %16.3f" % ml_sec_rate)
+    print("       ml per pl: %17.3f" % ml_in_pl)
+    print("       litres per minute: %9.3f" % lt_in_min)
+    print("       pulses per litre: %10.3f" % pl_in_lt)
     print("===========================================================")
 
+
+def handle_delayed_measuring(delay):
+    if delay > 0:
+        print("Waiting waterflow stopping for %s seconds:" % delay)
+        for s in range(delay, 1, -1): sleep(1 if delay > 1 else delay)
+    print()
+
+
+def calc_stat(dto):
+    time_elaps = dto.t2 - dto.t1
+    pl_in_sec = dto.pulse_count / time_elaps
+    lt_in_min = pl_in_sec / 15# 7.5 for single sensor state edge, 15 for both
+    ml_sec_rate = lt_in_min * (1000/60)
+    total_vol_ml = ml_sec_rate * time_elaps
+    return time_elaps, pl_in_sec, lt_in_min, ml_sec_rate, total_vol_ml
+
+
+def hall_pulse(dto, reached_event):
+    dto.pulse_count += 1
+    dto.t2 = timer()
+    stats = (calc_stat(dto))
+    if stats[4] >= dto.target_amount: reached_event.set()
+    if stats[0] > 0.001: print_statistics(dto.pulse_count, *stats)
     
-def main(flow_pin, button_pin, pump_pin):
-    global t1
+
+def print_statistics(pulse_count, time_elaps, pl_in_sec, lt_in_min, ml_sec_rate, total_vol_ml):
+    s = "  -> current speed: {:.3f}L/min|{:.3f}ml/sec|{:.3f}pl/sec; vol.estim: {:.3f}ml; pulses: {}; seconds: {:.3f}."\
+        .format(lt_in_min, ml_sec_rate, pl_in_sec, total_vol_ml, pulse_count, time_elaps)
+    print(s, flush=True, end='\r')
+
+
+def main(device_args, pump_speed=1, flow_stop_dely=0, target_amount=100):
+    dto = DTO(pulse_count = 0,
+              t1 = None,
+              t2 = None,
+              target_amount = target_amount)
+    amount_reached = Event()
+    #~ kbd_t = Thread(target=kbd_waiter, args=(amount_reached,), daemon=True)
+    kbd_t = Thread(target=input, daemon=True)
     try:
-        setup_gpio(flow_pin, button_pin, pump_pin)
-        print("Push the button to start executing")
-        button.wait_for_press()
-        print("-> Starting waterflow.")
-        pump.on()
-        t1 = time()
-        stop_evt.wait()
-        pump.off()
-        wait_for = 4
-        print("Waiting waterflow stopping for %s seconds:" % wait_for)
-        for s in range(wait_for, 1, -1):
-            #~ print("%s seconds left..." % s, flush=True, end='\r')
-            sleep(1 if wait_for > 1 else wait_for)
-        print()
+        sensor_counter_cb = lambda: hall_pulse(dto, amount_reached)
+        with Devices(dto, sensor_counter_cb, device_args) as devices:
+            input("Press Enter to start measuring. To stop measuring press Enter again.")
+            print("-> Starting waterflow.")
+            devices.vcc.on()
+            devices.valve.on()
+            devices.pump.value = pump_speed
+            dto.t1 = timer()
+            kbd_t.start()
+            while not amount_reached.wait(0.1):
+                if not kbd_t.is_alive(): break
+            else:
+                print("\n>>> target reached: %s ml" % calc_stat(dto)[4])
+            devices.pump.off()
+            devices.valve.off()
+            handle_delayed_measuring(flow_stop_dely)
     except KeyboardInterrupt:
-        print("Exiting gracefully...")
-    else:
-        if pulse_count > 0: handle_result()
-        else: print("No pulses read!  ")    
-    finally:
-        GPIO.remove_event_detect(flow_pin)
-        GPIO.cleanup((flow_pin, button_pin, pump_pin))
+        print()
+        print("Keyboard exit...")
+        return
+    if kbd_t.is_alive():
+        print(">> Press Enter to proceed...")
+    if dto.pulse_count > 0: handle_result(dto)
+    else: print("No pulses read!  ")
     print("End executing")
 
 
 if __name__ == '__main__':
-    main(6, 13, 24)
-
+    device_args = DTO(flow_pin=6,
+                      vcc_pin=25,
+                      valve_pin=18,
+                      pump_pin=24,
+                      button_pin=13)
+    process_args = dict(pump_speed=1,
+                        flow_stop_dely=0,
+                        target_amount=1000)
+    main(device_args, **process_args)
+    #~  5.21 == 1.112
+    #~  18.83 == 0.868

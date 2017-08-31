f63ae2d04ff7619ca837cdd547cd470aca02db61
Lauri Saar <lauri.saar@eesti.ee>
2017-08-31 21:47:13 +0300

    Fixed mistakes: - configuration module has still references water tank valve; - hardware module has still reference for LevelSensor which is no removed altogether from program. Small code style refactorings.

--- commandline_app/config.py
+++ commandline_app/config.py
@@ -5,41 +5,43 @@ def load_cfg_prod():
                      probe_full_pin=21,
                      led_low_pin=17,
                      led_norm_pin=27,
-                     led_full_pin=22,
-                     valve_pin=None)
+                     led_full_pin=22)
     plants_args = (
-        dict(id="JoodikLill",
-             sensor_vcc_pin=25,
-             valve_pin=18,
-             led_pin=23,
-             button_pin=13,
-             moist_percent=55,
-             watering_time=5,
-             pump_power=0.3,
-             device=1),
+        dict(
+              id="JoodikLill",
+              sensor_vcc_pin=25,
+              valve_pin=18,
+              led_pin=23,
+              button_pin=13,
+              moist_percent=55,
+              watering_time=5,
+              pump_power=0.3,
+              device=1
+              ),
         )
     return gardener_args, tank_args, plants_args
 ########################################################################
 def load_cfg_test1():
     gardener_args = dict(pump_pin=24, watch_cycle=25, watering_cycle=15)
-    tank_args = dict(probe_low_pin=16,
-                     probe_norm_pin=20,
-                     probe_full_pin=21,
-                     led_low_pin=17,
-                     led_norm_pin=27,
-                     led_full_pin=22,
-                     valve_pin=None,
-                     standby_interval=12,
-                     empty_interval=5)
+    tank_args = dict(
+       probe_low_pin=16,
+       probe_norm_pin=20,
+       probe_full_pin=21,
+       led_low_pin=17,
+       led_norm_pin=27,
+       led_full_pin=22
+       )
     plants_args = (
-        dict(id="JoodikLill",
-             sensor_vcc_pin=25,
-             valve_pin=18,
-             led_pin=23,
-             button_pin=13,
-             moist_percent=55,
-             watering_time=5,
-             pump_power=0.25,
-             device=1),
+        dict(
+              id="JoodikLill",
+              sensor_vcc_pin=25,
+              valve_pin=18,
+              led_pin=23,
+              button_pin=13,
+              moist_percent=55,
+              watering_time=5,
+              pump_power=0.25,
+              device=1
+              ),
         )
     return gardener_args, tank_args, plants_args
--- commandline_app/hardware/__init__.py
+++ commandline_app/hardware/__init__.py
@@ -1,6 +1,5 @@
 from .motor import UnidirectionMotor
 from .pumping import Pump
 from .soil_sensor import SoilSensor
-from .water_level_sensor import LevelSensor
 from .switch import Switch
 from .valve import Valve

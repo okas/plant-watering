f80fde68d6caf0628be2c7c70dc99f0df5094216
Lauri Saar <lauri.saar@eesti.ee>
2017-09-04 17:53:22 +0300

    Simplified and optimized stoppable_sleep: - waiting (sleep) time and internal timeout counter step is 0.2=>0.1, responsiveness didn't suffer; - predicate is called immediately if user's timeout is less then step size 0.1.

--- commandline_app/common/stop_tools.py
+++ commandline_app/common/stop_tools.py
@@ -2,20 +2,19 @@ from time import sleep
 
 
 def stoppable_sleep(time, predicate):
-    if time == 0: return False
-    elif time < 1:
-        if predicate(): return True
-        sleep(time)
-        return False
+    step = 0.1
+    if time < step:
+        return predicate()
     count_to_time = 0
-    step = 0.2
     while (count_to_time + step) < time:
         #print("looping...; predicate(): %s"%predicate())
-        if predicate(): return True
+        if predicate():
+            return True
         sleep(step)
         count_to_time += step
     else:
         #print("looping...; predicate(): %s"%predicate())
-        if predicate(): return True
+        if predicate():
+            return True
         sleep(time - count_to_time)
     return False

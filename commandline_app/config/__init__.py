daf1e1d845d0e4fb4e9ba4021dab5f2c91da9f2a
Lauri Saar <lauri.saar@eesti.ee>
2017-09-23 01:42:09 +0300

    Configuration returns now SimpleNamespace instance, instead of dict.

--- commandline_app/config/__init__.py
+++ commandline_app/config/__init__.py
@@ -1,6 +1,6 @@
 import os
 import json
-
+from types import SimpleNamespace
 
 def load_configuration(config_name):
     if '/' in config_name:
@@ -8,5 +8,8 @@ def load_configuration(config_name):
     else:
         file_path = os.path.abspath('%s/../%s.json' % (__file__, config_name))
     with open(file_path) as j_file:
-        raw_loaded = json.load(j_file)
+        raw_loaded = json.load(
+        j_file,
+        object_hook = lambda d: SimpleNamespace(**d)
+        )
     return raw_loaded

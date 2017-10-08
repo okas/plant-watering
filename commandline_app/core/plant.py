7ea8ac6d407f900f5108f843b12385a48316c46b
Lauri Saar <lauri.saar@eesti.ee>
2017-10-09 00:46:15 +0300

    Replaced BoundedSemaphore to Lock. Added lock context manager based executin to state.setter to avoid rac condition in led setting.

--- commandline_app/core/plant.py
+++ commandline_app/core/plant.py
@@ -1,6 +1,6 @@
 from enum import Enum, unique
 from timeit import default_timer as timer
-from threading import BoundedSemaphore
+from threading import Lock
 from gpiozero import PWMLED
 from hardware import SoilSensor
 from common import common_logger as log, stoppable_sleep
@@ -26,23 +26,25 @@ class Plant:
             pour_millilitres=50,
             **spi_args
             ):
-        self.id = id
-        self.led = PWMLED(led_pin, frequency=100)
-        self.valve_pin = valve_pin
-        self.sensor = SoilSensor(sensor_vcc_pin, **spi_args)
-        self.state = State.resting
-        self.moist_level = moist_percent
         self.stop_event = stop_event
+        self.id = id
+        self.moist_level = moist_percent
+        self.valve_pin = valve_pin
         self.pour_millilitres = pour_millilitres
-        self.__measuring_semaphore = BoundedSemaphore(value=1)
+        self.led = PWMLED(led_pin, frequency=100)
+        self.sensor = SoilSensor(sensor_vcc_pin, **spi_args)
+        self.__state = State.resting
+        self.__p_state = None
+        self.__measuring_lock = Lock()
+        self.__state_lock = Lock()
 
     def __del__(self):
         if hasattr(self, 'closed') and not self.closed:
             self.close()
 
     def measure(self, retain_state=False) -> tuple:
-        with self.__measuring_semaphore:
-            old_state = self.state
+        with self.__measuring_lock:
+            self.__p_state = self.state
             self.state = State.measuring
             moist = self.sensor.moisture_percent
             if moist > self.moist_level:
@@ -52,19 +54,20 @@ class Plant:
                 new_state = State.needs_water
                 result = True
             else:
-                log("id: %s, state: %s, moisture: %s, sensor: %s"\
+                log(' %s, state: %s, moisture: %s, sensor: %s'
                     % (self.id, self.state, self.moist_level, moist))
-                raise Exception("Should not end up here! Something is "\
-                                "wrong with Plant instance state handling.")
-            self.state = old_state if retain_state else new_state
-            return (result, moist)
+                raise Exception('Should not end up here! Something is '\
+                                'wrong with Plant instance state handling.')
+            self.state = self.__p_state if retain_state else new_state
+            self.__p_state = None
+        return (result, moist)
 
     def close(self):
         self.closed = False
         self.led.close()
         self.sensor.close()
         self.closed = True
-        log("Closed %s." % self.id)
+        log('Closed %s.' % self.id)
 
     @property
     def state(self) -> State:
@@ -72,8 +75,9 @@ class Plant:
 
     @state.setter
     def state(self, val):
-        self.__state = val
-        if isinstance(val.value, tuple):
-            self.led.blink(*val.value)
-        else:
-            self.led.value = val.value
+        with self.__state_lock:
+            self.__state = val
+            if isinstance(val.value, tuple):
+                self.led.blink(*val.value)
+            else:
+                self.led.value = val.value

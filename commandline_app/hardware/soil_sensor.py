7d8e55bf78dd62f8fcea1730fe110131b765d020
Lauri Saar <lauri.saar@eesti.ee>
2017-10-31 01:19:33 +0200

    Soil sensor renewed: - uses now capacitive physical sensor, - cap. sensor uses reversed valuses. - added calibration methods and relevant default values

--- commandline_app/hardware/soil_sensor.py
+++ commandline_app/hardware/soil_sensor.py
@@ -1,63 +1,67 @@
-from gpiozero import MCP3008, OutputDevice
 from time import sleep
 from timeit import default_timer as timer
 from statistics import median
+from gpiozero import MCP3008, OutputDevice
+import sys
+import os
+sys.path.insert(1, os.path.abspath(__file__+'/../..'))
 from common import common_logger as log
 
 
-class SoilSensor():
-    # Excpecting chip's Vdd +5V
-    # todo: calculate spi value specifics based on voltages that consumer can provide
-
-    # At the most moistured soli values are as following:
-    # - soil moisture sensor 3V3; 0.33A; sensor resistance is about R10 (lowest)
-    __val_max = 0.782
-    __vref = 3.3
-    __vref_raw_max = 800
-
-    def __init__(self, vcc_pin, reads=250, wait_for_seconds=0.01, **spi_args):
-        self.__spi_args = spi_args
-        self.__spi_dev = MCP3008(**spi_args)
+class CapacitiveSensor():
+    def __init__(
+            self,
+            vcc_pin,
+            spi_device,
+            spi_channel,
+            dry_value,
+            wet_value,
+            measuring_samples=40):
+        self.closed = False
+        self.__spi_dev = MCP3008(device = spi_device, channel = spi_channel)
         self.__spi_vcc = OutputDevice(vcc_pin)#using GPIO pin to power the Sensor!
-        self.reads = reads
-        self.wait_for_seconds = wait_for_seconds
+        self.dry_value = dry_value
+        self.wet_value = wet_value
+        self.samples = measuring_samples
 
     def __del__(self):
-        if hasattr(self, 'closed') and not self.closed: self.close()
+        if not self.closed:
+            self.close()
 
-    def __read_iter(self):
-        for i in range(self.reads):
+    def __read_iter(self, samples = None):
+        for i in range(samples or self.samples):
+            sleep(0.01)
             v = self.__spi_dev.value
-            sleep(self.wait_for_seconds)
             yield v
 
-    '''
-    Multiple readigns are usuallt better. Calculate some avarag or median out of list of results
-    :param int reads:
-        Times to read sensor.
-    :param float wait_for_seconds:
-        Waiting time between reads.
-    '''
-    def read(self):
+    def calibrate_dry(self, samples = None):
+        self.dry_value = median(self.read_samples(samples))
 
-        log("  Sensor is on.")
-        values =[]
-        t1 = timer()
+    def calibrate_wet(self, samples = None):
+        self.wet_value = median(self.read_samples(samples))
+
+    def read_samples(self, samples = None):
         self.__spi_vcc.on()
-        values = [i for i in self.__read_iter()]
+        sleep(0.2)
+        values = list(self.__read_iter(samples))
         self.__spi_vcc.off()
-        t2 = timer()
-        log("  Sensor is off; read {:d} values in {:.3f} seconds.".format(self.reads, (t2 - t1)))
         return values
-
-    def close(self):
-        self.closed = False
-        self.__spi_dev.close()
-        self.__spi_vcc.close()
-        self.closed = True
 
     @property
     def moisture_percent(self):
-        result = 100 * median(self.read()) / SoilSensor.__val_max
-        log("  Sensor moisture median: {:.2f}%.".format(result))
+        t1 = timer()
+        readings = self.read_samples()
+        t2 = timer()
+        elaps = t2 - t1
+        v = median(readings)
+        ratio = (v - self.wet_value) / (self.dry_value - self.wet_value)
+        ratio_reversed = 1 - ratio
+        result = ratio_reversed * 100
+        log("  Sensor moisture median: [{:.2f}%], {:d} samples in {:.3f} seconds)."\
+            .format(result, len(readings), elaps))
         return result
+
+    def close(self):
+        self.__spi_dev.close()
+        self.__spi_vcc.close()
+        self.closed = True

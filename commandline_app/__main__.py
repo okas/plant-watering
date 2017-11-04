ecbe01097a09bc0180d8457e8356a4c70994130e
Lauri Saar <lauri.saar@eesti.ee>
2017-11-04 21:14:44 +0200

    Refactored main package __main__ module.

--- commandline_app/__main__.py
+++ commandline_app/__main__.py
@@ -1,31 +1,56 @@
-import argparse
-import os
-import __init__
+from version import __version__
 
-config_dir = os.path.abspath(__file__+'/../configuration/')
-configs = [x[:-5] for x in os.listdir(config_dir) if x.endswith('.json')]
-if 'default' in configs:
-    default_choice = 'default'
-elif 'prod' in configs:
-    default_choice = 'prod'
-elif len(configs) > 0:
-    default_choice = configs[0]
-else:
-    raise Exception(
-        'Expected "default" or "prod" config existence in ../config/ !\n'\
-        'Please add file using example file "default.json.example".\n'\
-        'If valid file already exists then just rename one of them.'
+import os
+import argparse
+
+def get_config_choises():
+    config_dir = os.path.abspath(__file__+'/../configuration/')
+    configs = [x[:-5] for x in os.listdir(config_dir) if x.endswith('.json')]
+    if len(configs) == 0:
+        raise Exception(
+            'Expected some *.json files in ../configuration/ !\n'\
+            'Please add file using example file "default.json.example".'
+            )
+    for e in ('default', 'production', 'prod', '/'):
+        if e != '/':
+            try:
+                default_index = configs.index(e)
+            except:
+                continue
+        else:
+            default_index = 0
+        if default_index > -1:
+            break
+    return (configs, default_index)
+
+
+def get_argument_data(config_choices, default_choise_index):
+    parser = argparse.ArgumentParser(
+        description='Lets water our plants!'
         )
-parser = argparse.ArgumentParser(
-    description='Lets water our plants!'
-    )
-parser.add_argument(
-    '-c', '--config',
-    choices=configs,
-    default=default_choice,
-    required=False,
-    help="Default is [%(default)s]. Program configuration."\
-    "There is selection of *.json file names, that where found from ../config/. "\
-    "You must fill in configurations before program can run!"
-    )
-__init__.run_commandline(parser.parse_args().config)
+    parser.add_argument(
+        '-c', '--config',
+        choices=config_choices,
+        default=config_choices[default_choise_index],
+        required=False,
+        help="Default is [%(default)s]. Program configuration."\
+        "There is selection of *.json file names, that where found "\
+        "from ../configuration/. "\
+        "You must fill in configurations before program can run!"
+        )
+    return parser.parse_args()
+
+
+def main():
+    choises_data = get_config_choises()
+    parsed_arguments = get_argument_data(*choises_data)
+    from __init__ import run_commandline
+    run_commandline(parsed_arguments.config)
+
+
+if __name__ == '__main__':
+    main()
+
+del os
+del argparse
+

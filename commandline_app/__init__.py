0bfc1e6ee5fbf31c9e3bceea3ff5f51e01f1ee1d
Lauri Saar <lauri.saar@eesti.ee>
2017-11-05 19:03:25 +0200

    Starting logging.exception() implementation.

--- commandline_app/__init__.py
+++ commandline_app/__init__.py
@@ -12,29 +12,28 @@ def run_and_return(config_name_or_path):
 
 
 def run_commandline(config):
-    from logging import debug, info
+    import logging
     gardener = None
     _err = None
     try:
         gardener = Gardener(config)
         gardener.stop_event.wait()
-    except (KeyboardInterrupt):
-        debug("! Received keyboard interrupt.\n")
+    except KeyboardInterrupt:
+        logging.debug("! Received keyboard interrupt.\n")
     except SystemExit as err:
-        debug("Someting wants to SystemExit...\n")
+        logging.exception("Someting wants to SystemExit...\n")
         _err = err
-    except Exception as err:
-        debug("Encountered some exeption, should see it after "\
+    except BaseException as err:
+        logging.exception("Encountered some exeption, should see it after "\
             "'Program done' message below.\n")
         _err = err
     finally:
         if gardener is not None:
             gardener.__del__()
-    info("Program done.\n")
+    logging.info("Program done.\n")
     if _err is not None:
-        debug("Re-raised error, that occured during program execution:\n")
+        logging.exception("Re-raised error, that occured during program execution:\n")
         raise _err
 
 del os
 del sys
-

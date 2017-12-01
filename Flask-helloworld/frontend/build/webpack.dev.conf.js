6c7a4ecfd9aff77c607de1b051dbd0466ef66513
Lauri Saar <lauri.saar@eesti.ee>
2017-12-01 21:38:06 +0200

    Moved favicon.ico to correct place and fixed configuration as well.

--- Flask-helloworld/frontend/build/webpack.dev.conf.js
+++ Flask-helloworld/frontend/build/webpack.dev.conf.js
@@ -47,6 +47,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
+      favicon: 'favicon.ico',
       inject: true
     }),
   ]

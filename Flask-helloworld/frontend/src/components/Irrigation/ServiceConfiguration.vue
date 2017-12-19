7dacd5175afd55e03fab79dc550484efdcdfc03a
Lauri Saar <lauri.saar@eesti.ee>
2017-12-19 02:39:39 +0200

    frontend/Irrigation/ServiceConfiguration.vue: fet: Added ability to save config files. chg: 1. Imporved API communication regarding varios HTTP responses handling. 2. removed <pre> tag 3. removed values from a[href] 4. Added guards for various states controles against in JSON tree component and other states.

--- Flask-helloworld/frontend/src/components/Irrigation/ServiceConfiguration.vue
+++ Flask-helloworld/frontend/src/components/Irrigation/ServiceConfiguration.vue
@@ -3,40 +3,48 @@
     <header>
         <h3>Irrigation service cofiguration</h3>
         <p>
-            You can evaluate and change configuration.
+            You can evaluate and change configuration.<br/>
+            Service restart will be done only if service is started already.
+            <br/> <code>TODO: disable save link, when no config loaded</code>
         </p>
         <ul class="list-inline">
             <li v-if="status" v-text="status"></li>
         </ul>
     </header>
     <ul class="list-inline activities">
-        <li><a href="#" @click.prevent="reload" v-text="state"></a></li>
         <li><a
-                href="#"
+            href=""
+            @click.prevent="apiGetServiceConfig"
+            v-text="state">
+        </a></li>
+        <li><a
+                href=""
                 @click.prevent="modify = !modify"
                 :class="stateClass">
                 modify
-            </a></li>
-        <li><a href="#" @click.prevent="reload">save and restart service</a></li>
+        </a></li>
+        <li v-if="hasConf"><a href="" @click.prevent="apiUpdateServiceConfig">
+            save and restart service
+        </a></li>
     </ul>
     <tree-view
-        v-if="configData !== ''"
+        v-if="hasConf"
         :data="configData.content"
         :options="{
             rootObjectKey: configData.filename,
             maxDepth: 3,
             modifiable: this.modify}"
         @change-data="onChangeData"/>
-    <hr/>
-    <pre v-text="configData"></pre>
 </article>
 </template>
 
 <script>
-import axios from 'axios'
 import Vue from 'vue'
+import axios from 'axios'
 import TreeView from 'vue-json-tree-view'
 Vue.use(TreeView)
+
+const apiConf = { headers: { 'Content-Type': 'application/json' } }
 
 export default {
     name: 'IrrigationServiceConfiguration',
@@ -48,24 +56,39 @@ export default {
         }
     },
     methods: {
-        reload () {
-            this.modify = false
-            this.apiGetServiceConfig()
-        },
         onChangeData (updatedDocument) {
             this.configData.content = updatedDocument
         },
         apiGetServiceConfig () {
+            this.modify = false
             axios.get('/api/irrigation/service-config')
                 .then(resp => {
                     this.status = ''
                     this.configData = resp.data
                 })
                 .catch(console.log)
+        },
+        apiUpdateServiceConfig () {
+            this.modify = false
+            let filename = this.configData.filename || ''
+            let api = `/api/irrigation/service-config/${filename}/update-restart`
+            axios.put(api, this.configData.content, apiConf)
+                .then(resp => {
+                    this.status = resp.status !== 204 ? resp.data.message : ''
+                })
+                .catch((err) => {
+                    if (!this.inProduction) {
+                        this.status = err.response.data.message
+                        console.log(err)
+                    }
+                })
         }
     },
     computed: {
-        stateClass: function () {
+        hasConf () {
+            return this.configData
+        },
+        stateClass () {
             if (this.modify) {
                 return 'highlight'
             } else if (!this.modify) {
@@ -74,10 +97,11 @@ export default {
                 return 'error'
             }
         },
-        state: function () {
-            return !this.configData
-                ? 'load from server'
-                : 'reload from server'
+        state () {
+            return !this.hasConf ? 'load from server' : 'reload from server'
+        },
+        inProduction () {
+            return process.env.NODE_ENV === 'production'
         }
     }
 }

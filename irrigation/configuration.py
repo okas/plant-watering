70f37ab41f948382d88f360131594258c09b7bcf
Lauri Saar <lauri.saar@eesti.ee>
2017-12-19 02:17:13 +0200

    irrigation/configuration.py module: chg: Dropped using namedtuple class, didn't see value anymore. Motivation was to guarantee JSON content reading exact order in object.
    irrigation/gardener and plant modules:
    chg: These are consumers on configuration -- applied necessary changes.

--- irrigation/configuration.py
+++ irrigation/configuration.py
@@ -1,24 +1,15 @@
 import os
 import json
-from collections import namedtuple
-
-
-def recursive_converter(obj):
-    if isinstance(obj, dict):
-        for key, val in obj.items():
-            obj[key] = recursive_converter(val)
-        return namedtuple('GenericConfig', obj.keys())(**obj)
-    elif isinstance(obj, list):
-        return [recursive_converter(item) for item in obj]
-    else:
-        return obj
+from collections import OrderedDict
 
 
 def load_configuration(name_or_path):
     if '/' or '.json' in name_or_path:
         file_path = name_or_path
     else:
-        file_path = os.path.realpath('%s/../%s.json' % (__file__, name_or_path))
+        file_path = os.path.realpath(
+            '{}/../{}.json'.format(__file__, name_or_path)
+            )
     with open(file_path) as j_file:
-        config = json.load(j_file, object_hook = recursive_converter)
-    return config
+        ordered_config = json.load(j_file, object_pairs_hook = OrderedDict)
+    return ordered_config

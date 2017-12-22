ca4c31fcf30f201b295b957260d27aec38402db8
Lauri Saar <lauri.saar@eesti.ee>
2017-12-22 02:08:27 +0200

    irrigation/configuration: chg: 1. Dropped own recursive config parser for OrderedDict object_pair_hook fet: 1. Implemented jsonschema package 2. Generated schema for current config: https://jsonschema.net/# 2.1 Quite default settings nad not finetuned!

--- irrigation/configuration.py
+++ irrigation/configuration.py
@@ -1,17 +1,37 @@
 import os
-import json
-from collections import namedtuple
+import logging
+import simplejson as json
+from jsonschema import Draft4Validator
+from collections import OrderedDict
 
 
-def recursive_converter(obj):
-    if isinstance(obj, dict):
-        for key, val in obj.items():
-            obj[key] = recursive_converter(val)
-        return namedtuple('GenericConfig', obj.keys())(**obj)
-    elif isinstance(obj, list):
-        return [recursive_converter(item) for item in obj]
-    else:
-        return obj
+log = logging.getLogger(__name__)
+
+
+def _load_schema():
+    path = os.path.realpath(
+        os.path.dirname(__file__)+'/configuration.schema'
+        )
+    with open(path) as s_file:
+        schema = json.load(
+            s_file,
+            object_pairs_hook = OrderedDict,
+            encoding='utf-8'
+            )
+    try:
+        Draft4Validator.check_schema(schema)
+    except SchemaError as err:
+        log.exception("Irrigation configuration schema itself is bad!")
+    return schema
+
+
+def validate(instance):
+    validator = Draft4Validator(_load_schema())
+    if validator.is_valid(instance):
+        return
+    for err in validator.iter_errors(instance):
+        log.debug(err.message)
+    raise Exception()
 
 
 def load_configuration(name_or_path):
@@ -20,5 +40,13 @@ def load_configuration(name_or_path):
     else:
         file_path = os.path.realpath('%s/../%s.json' % (__file__, name_or_path))
     with open(file_path) as j_file:
-        config = json.load(j_file, object_hook = recursive_converter)
-    return config
+        ordered_config = json.load(j_file, object_pairs_hook = OrderedDict)
+    try:
+        validate(ordered_config)
+    except:
+        log.exception('Loaded Irrigation configuration is not valid! '\
+            'File: %s' % file_path)
+    else:
+        log.debug('Loaded Irrigation configuration is valid. '\
+            'File: %s' % file_path)
+    return ordered_config

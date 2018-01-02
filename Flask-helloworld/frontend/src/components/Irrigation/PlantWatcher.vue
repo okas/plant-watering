116282515810296d4473ae5b72ca68314fc003ab
Lauri Saar <lauri.saar@eesti.ee>
2018-01-03 01:16:34 +0200

    enh: 1. Applied some nef application level style classes 2. If service is unavailable then PlantWatcher view 'disables' visually plants 2.1. Plant activities are disabled as well: refresh, statistics, an calibration.
    chg: 1. Plant loading changes:
    1.1. Using add or update logic, because as soon as service starts then every plant changes its state.
    States are immediately broadcasted. As such, on PlantWatcher component reload
    there can be situation where plant array and state update for same plant arrives.
    Desided to bee a bit more efective and using both data. Distinction is based on name property.

--- Flask-helloworld/frontend/src/components/Irrigation/PlantWatcher.vue
+++ Flask-helloworld/frontend/src/components/Irrigation/PlantWatcher.vue
@@ -2,42 +2,57 @@
 <layout>
 <section>
     <header>
-        <h2>'Real-time' plant overview</h2>
-        <ul class="list-inline">
-            <li v-if="status" v-text="status"></li>
-        </ul>
+        <h2>Real-time plant overview</h2>
     </header>
     <article>
+        <header>
+            <ul class="list-style-none">
+                <li class="status-small">
+                    Updates: <span v-text="ticker"/>
+                    </li>
+                <li v-if="status" v-text="status" :class="statusClass"/>
+            </ul>
+        </header>
         <ul class="plant-list clearfix">
-            <dl class="plant-block" v-for="p in plants">
-                <dt class="h1" v-text="p.name"></dt>
-                <div class="horizontal status">
-                    <dt>state:</dt>
-                    <dd v-text="p.state"></dd>
-                </div>
-                <div class="horizontal">
-                    <dt class="h2">Moisture</dt>
-                    <dd class="h2">%</dd>
-                </div>
-                <div>
+            <dl v-for="p in plants" :class="stateOutterClass" class="plant-block">
+                <div :class="stateInnerClass">
+                    <dt class="h1" v-text="p.name"></dt>
                     <div class="horizontal status">
-                        <dt>required:</dt>
-                        <dd v-text="p.moist_level"></dd>
-                    </div>
-                    <div class="horizontal status">
-                        <dt>measured:</dt>
-                        <dd v-text="p.moist_measured"></dd>
+                        <dt>state:</dt>
+                        <dd v-text="p.state"></dd>
                     </div>
                     <div class="horizontal">
-                        <a href="" @click.prevent="wsRefreshPlant(p)">refresh</a>
-                        <span>&nbsp;|&nbsp;</span>
-                        <router-link :to="{name: 'plantstats', params: {name: p.name}}">
-                            stats
-                        </router-link>
-                        <span>&nbsp;|&nbsp;</span>
-                        <router-link :to="{name: 'plantcalibrate', params: {name: p.name}}">
-                            calibrate
-                        </router-link>
+                        <dt class="h2">Moisture</dt>
+                        <dd class="h2">%</dd>
+                    </div>
+                    <div>
+                        <div class="horizontal status">
+                            <dt>required:</dt>
+                            <dd v-text="p.moist_level"></dd>
+                        </div>
+                        <div class="horizontal status">
+                            <dt>measured:</dt>
+                            <dd v-text="p.moist_measured"></dd>
+                        </div>
+                        <div class="horizontal">
+                            <a href=""
+                                v-if="serviceIsOn"
+                                v-text="linkRef"
+                                @click.prevent="wsRefreshPlant(p)"/>
+                            <span v-text="linkRef" v-else/>
+                            <span>&nbsp;|&nbsp;</span>
+                            <router-link
+                                v-if="serviceIsOn"
+                                v-text="linkSta"
+                                :to="{name: 'plantstats', params: {name: p.name}}"/>
+                            <span v-text="linkSta" v-else/>
+                            <span>&nbsp;|&nbsp;</span>
+                            <router-link
+                                v-if="serviceIsOn"
+                                v-text="linkCal"
+                                :to="{name: 'plantcalibrate', params: {name: p.name}}"/>
+                            <span v-text="linkCal" v-else/>
+                        </div>
                     </div>
                 </div>
             </dl>
@@ -56,47 +71,107 @@ export default {
     head: { title: { inner: 'Plant watcher' } },
     data () {
         return {
-            status: 'loading plants from server...',
-            plants: []
+            status: '...loading plants from server...',
+            serviceIsOn: false,
+            plants: [],
+            watcherIsRendering: false,
+            statusClass: 'highlight-disa',
+            stateOutterClass: '',
+            stateInnerClass: '',
+            ticker: 0,
+            linkRef: 'refresh',
+            linkSta: 'stats',
+            linkCal: 'calibrate'
         }
     },
     sockets: {
         update_plant_status (data) {
-            const fraction = 'server initiated plant status update'
             if (!data) {
-                this.status = `${fraction}, but no data was sent.`
+                this.status = 'server initiated plant status update, but no data was sent.'
+                this.statusClass = 'highlight-crit'
                 return
             }
-            var plant = this.plants.find(p => p.name === data.name)
-            if (!plant) {
-                this.status = `${fraction}, didn't found plant with name "${data.name}"'`
-                return
+            if (this.plants.length === 0) {
+                this.plants.push(data)
+                this.stateOutterClass = 'high'
+                this.stateInnerClass = 'default-text-color'
+            } else {
+                this.addOrUpdatePlant(data)
             }
-            Object.assign(plant, data)
             this.status = ''
+            this.ticker++
+        }
+    },
+    watch: {
+        '$store.state.irrigation.statusObj.state': {
+            handler (state) {
+                if (state === 'on') {
+                    this.serviceIsOn = true
+                    this.stateOutterClass = 'high'
+                    this.stateInnerClass = 'default-text-color'
+                    this.statusClass = ''
+                    this.status = ''
+                    this.renderPlants()
+                } else {
+                    this.serviceIsOn = false
+                    this.stateInnerClass = 'disa'
+                    this.stateOutterClass = 'disa'
+                    this.statusClass = 'highlight-warn'
+                    this.status = 'Service is not running, cannot update.'
+                }
+            }
         }
     },
     methods: {
         wsRefreshPlant (plant) {
             this.$socket.emit('get_plant_status', plant.name, (data) => {
                 if (!data) {
-                    this.status = `didn't get refresh for "${plant.name}", check what's wrong.`
+                    if (!this.watcherIsRendering) {
+                        this.status = `didn't get refresh for "${plant.name}", check what's wrong.`
+                    }
                     return
                 }
                 Object.assign(plant, data)
                 this.status = ''
             })
+        },
+        renderPlants () {
+            this.watcherIsRendering = true
+            this.$socket.emit('get_watcher_state', (data) => {
+                if (data && 'error' in data) {
+                    this.status = data.error
+                    this.statusClass = 'highlight-crit'
+                    return
+                }
+                // TODO: plants on loading situation
+                // ## load 1x1 -- server must notify at first(or additional argument) how much plants will come.
+                if (!Array.isArray(data) || data.length === 0) {
+                    this.status = "didn't get any plants, check what's wrong'"
+                    return
+                }
+                data.forEach(this.addOrUpdatePlant)
+                this.status = ''
+                this.stateOutterClass = 'high'
+                this.stateInnerClass = 'default-text-color'
+                this.watcherIsRendering = false
+            })
+        },
+        addOrUpdatePlant (plant) {
+            var existing = this.plants.find(p => p.name === plant.name)
+            if (existing) {
+                Object.assign(existing, plant)
+            } else {
+                this.plants.push(plant)
+            }
         }
     },
-    beforeMount () {
-        this.$socket.emit('get_watcher_state', (data) => {
-            if (!Array.isArray(data) || data.length === 0) {
-                this.status = "didn't get any plants, check what's wrong'"
-                return
-            }
-            this.plants = data
-            this.status = ''
-        })
+    created () {
+        if (this.$store.state.irrigation.statusObj.state !== 'on') {
+            this.statusClass = 'highlight-warn'
+            this.status = 'Service is not running, can\'t render the overview for you.'
+            return
+        }
+        this.renderPlants()
     }
 }
 </script>
@@ -104,11 +179,13 @@ export default {
 <style scoped>
 .plant-list {
     padding: 0;
+    margin: 0;
 }
 .plant-block {
     width: 195px;
-    box-shadow: -1px -1px 4px 2px #52e4b585;
+    box-shadow: -1px -1px 4px 2px;
     display: inline-block;
+    margin: 0 25px 50px;
 }
 .h1 {
     font-size: 1.5em;

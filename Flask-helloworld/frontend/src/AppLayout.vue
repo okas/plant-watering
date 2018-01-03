439d62938a14bb354a0339ca038d96adad698e71
Lauri Saar <lauri.saar@eesti.ee>
2018-01-04 01:34:06 +0200

    enh: Added classes that float left and rigth and with text aligned to right with float right. chg: Core style refactoring

--- Flask-helloworld/frontend/src/AppLayout.vue
+++ Flask-helloworld/frontend/src/AppLayout.vue
@@ -48,7 +48,7 @@ body, .default-text-color {
     color: #3e0101;
 }
 h1, h2 {
-  font-weight: normal;
+    font-weight: normal;
 }
 h1 {
     font-size : 3em;
@@ -65,57 +65,67 @@ h3 {
     color : #00B945;
 }
 a {
-  color: #7e008c;
-}
-.clearfix:after {
-   content: "";
-   clear: both;
-   display: table;
+    color: #7e008c;
 }
 a.router-link-exact-active {
-   text-shadow: 0px 0px 20px #00f9ff;
+    text-shadow: 0px 0px 20px #00f9ff;
+}
+.clearfix:after {
+    content: "";
+    clear: both;
+    display: table;
+}
+.float-left {
+    float: left;
+}
+.float-right {
+    float: right;
+}
+.float-align-right {
+    float: right;
+    text-align: right;
 }
 .high, .info, .warn, .crit, .disa, .highlight-full, .highlight,
 .highlight-warn, .highlight-crit, .default-text-color
 {
-  transition: 0.5s color;
+    transition: 0.5s color;
 }
 .high {
-  color: #1deca0bd;
+    color: #1deca0bd;
 }
 .info {
-  color: #52e4b585;
+    color: #52e4b585;
 }
 .warn {
-  color: #e9ec00f0;
+    color: #e9ec00f0;
 }
 .crit {
-  color: #fb6c6cbd;
+    color: #fb6c6cbd;
 }
 .disa {
-  color: #c5beae;
+    color: #c5beae;
 }
 .highlight-full {
-  box-shadow: -1px -1px 1px 1px #1717e8bd;
+    box-shadow: -1px -1px 1px 1px #1717e8bd;
 }
 .highlight {
-  box-shadow: -1px -1px 1px 1px #1deca0bd;
+    box-shadow: -1px -1px 1px 1px #1deca0bd;
 }
 .highlight-warn {
-  box-shadow: -1px -1px 1px 1px #e9ec00f0;
+    box-shadow: -1px -1px 1px 1px #e9ec00f0;
 }
 .highlight-crit {
-  box-shadow: -1px -1px 1px 1px #fb6c6cbd;
+    box-shadow: -1px -1px 1px 1px #fb6c6cbd;
 }
 .highlight-disa {
-  box-shadow: -1px -1px 1px 1px #c5beae;
+    box-shadow: -1px -1px 1px 1px #c5beae;
 }
 .list-inline, .list-style-none {
     list-style-type: none;
     padding: 0;
 }
 .list-inline li {
-  display: inline-block;
+    display: inline-block;
 }
 .status-small {
     font-size: 0.6em;
